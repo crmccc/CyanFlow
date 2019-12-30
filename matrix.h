374a8db233437cf83a22891ba9f5034d50087c1d
crmccc <crmccc@126.com>
2019-12-30 22:01:51 +0800

    bugs in oprator= overload

--- matrix.h
+++ matrix.h
@@ -13,7 +13,9 @@ public:
     int x_length = 0;
     int y_length = 0;
     Matrix(int, int);
-    Matrix operator*(const Matrix &b);
+    Matrix operator*(const Matrix &);
+    void operator=(const Matrix &);
+    
     ~Matrix();
 };
 
@@ -22,19 +24,27 @@ public:
 //|
 //|
 //also fixed length
-Matrix Matrix::operator*(const Matrix &bM) 
-{    
-    Matrix res(bM.y_length, x_length);
-    complex<double> **b = bM.data;
-    complex<double> **a = data;
-    complex<double> **c = res.data;
-    for (int i = 0; i < x_length; i++)
+void Matrix::operator=(const Matrix &b){
+    for (int x = 0; x < x_length; ++x)
     {
-        for (int j = 0; j < bM.y_length; j++)
+        delete[] data[x];
+    }
+    delete[] data;
+    data=b.data;
+    x_length=b.x_length;
+    y_length=b.y_length;
+}
+
+Matrix Matrix::operator*(const Matrix &b) 
+{    
+    Matrix res(x_length,b.y_length);
+    for (int i = 0; i < res.x_length; i++)
+    {
+        for (int j = 0; j < res.y_length; j++)
         {
-            for (int k = 1; k <= y_length; a++)
+            for (int k = 1; k < y_length; k++)
             {
-                c[i][j] += a[i][k] * b[k][j];
+                res.data[i][j] = data[i][k] * b.data[k][j]+res.data[i][j];
             }
         }
     }
@@ -46,7 +56,7 @@ complex<double> **Matrix::make_a_matrix(int x, int y)
     complex<double> **res = new complex<double> *[x];
     for (int i = 0; i < y; ++i)
     {
-        res[i] = new complex<double>[y];
+        res[i] = new complex<double>[y]{0};
     }
 
     return res;
@@ -59,11 +69,6 @@ Matrix::Matrix(int x, int y)
     data = make_a_matrix(x, y);
     y_length = y;
 
-}
-
-Matrix::Matrix(int x, int y)
-{
-    data = make_a_matrix(x, y);
 }
 
 Matrix::~Matrix()
---
+++ testmatrix.cpp
@@ -0,0 +1,24 @@
+#include"matrix.h"
+#include<stdio.h>
+int main (){
+    Matrix a(100,100);
+    Matrix b(100,100);
+    a.data[0][0]=1;
+    a.data[0][1]=2;
+    a.data[1][0]=3;
+    a.data[1][1]=4;
+    b.data[0][0]=1;
+    b.data[0][1]=2;
+    b.data[1][0]=3;
+    b.data[1][1]=4;
+    Matrix c(100,100);
+    c=a*b;
+    printf("%f %f\n%f %f\n\n",a.data[0][0].real(),a.data[0][1].real(),a.data[1][0].real(),a.data[1][1].real());
+    printf("%f %f\n%f %f\n\n",b.data[0][0].real(),b.data[0][1].real(),b.data[1][0].real(),b.data[1][1].real());
+    printf("%f %f\n%f %f\n\n",c.data[0][0].real(),c.data[0][1].real(),c.data[1][0].real(),c.data[1][1].real());
+    
+    a.~Matrix();
+    b.~Matrix();
+    c.~Matrix();
+    return 0;
+}
