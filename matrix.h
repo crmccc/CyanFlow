9ee27ec7e51c3053e8d78a1fef4d74ef2a47889c
crmccc <crmccc@126.com>
2019-12-31 09:11:12 +0800

    abandon this

--- matrix.h
+++ matrix.h
@@ -1,6 +1,6 @@
 #include <complex>
 using std::complex;
-
+int numbers=0;
 class Matrix
 //a fix length ugly complex matrix
 {
@@ -12,10 +12,11 @@ public:
     complex<double> **data;
     int x_length = 0;
     int y_length = 0;
-    
+    int flag;
     Matrix operator*(const Matrix &);
     void operator=(const Matrix &);
     complex<double>*& operator[](int i);
+    int mutiplit(const Matrix &,const Matrix &,const Matrix &);
     
     Matrix(int, int);
     ~Matrix();
@@ -27,12 +28,17 @@ public:
 //|
 //also fixed length
 void Matrix::operator=(const Matrix &b){
+    //this* must bigger than b
     // for (int x = 0; x < x_length; ++x)
     // {
     //     delete[] data[x];
     // }
     // delete[] data;
-    data=b.data;
+    for (int x=0;x<x_length;++x){
+        for (int y=0;y<y_length;++y){
+            data[x][y]=b.data[x][y];
+        }
+    }
     x_length=b.x_length;
     y_length=b.y_length;
 }
@@ -50,7 +56,7 @@ Matrix Matrix::operator*(const Matrix &b)
             }
         }
     }
-    return res;
+    return (res);
 }
 complex<double>*& Matrix::operator[](int i){
     return data[i];
@@ -69,6 +75,8 @@ complex<double> **Matrix::make_a_matrix(int x, int y)
 
 Matrix::Matrix(int x, int y)
 {
+    numbers++;
+    flag=numbers;
     x_length = x;
     y_length = y;
     data = make_a_matrix(x, y);
@@ -76,10 +84,25 @@ Matrix::Matrix(int x, int y)
 
 Matrix::~Matrix()
 {
+    numbers--;
     for (int x = 0; x < x_length; ++x)
     {
-        delete[] data[x];
+       delete[] data[x];
     }
     delete[] data;
     data=NULL;
+}
+
+int Matrix::mutiplit(const Matrix &a,const Matrix &b,const Matrix &res){
+    for (int i = 0; i < res.x_length; i++)
+    {
+        for (int j = 0; j < res.y_length; j++)
+        {
+            for (int k = 0; k < y_length; k++)
+            {
+                res.data[i][j] = data[i][k] * b.data[k][j]+res.data[i][j];
+            }
+        }
+    }
+
 }
