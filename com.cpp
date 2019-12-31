36a621703c6a7b572c9522e632be5e8a25b675b9
crmccc <crmccc@126.com>
2019-12-31 09:21:03 +0800

    a,shit,what a fucking mass

---
+++ com.cpp
@@ -0,0 +1,7 @@
+#include<complex>
+#include<iostream>
+using namespace std;
+int main(){
+    complex<double> c1;
+    c1.
+}
--- testmatrix.cpp
+++ testmatrix.cpp
@@ -1,18 +1,17 @@
 #include"matrix.h"
 #include<stdio.h>
-void show_matrix(Matrix &a);
+void show_matrix(const Matrix &a);
 int main (){
     Matrix a(100,100);
     Matrix b(100,100);
     Matrix c(100,100);
-    c=a*b;
     for (int i=0;i<2;++i){
         for(int j=0;j<2;++j){
             a.data[i][j]=i*j+1;
             b.data[i][j]=i*j+1;
         }
     }
-    c=a*b;
+    c.mutiplit(a,b,c);
     show_matrix(c);
 
     a.~Matrix();
@@ -21,7 +20,7 @@ int main (){
     return 0;
 }
 
-void show_matrix(Matrix &a){
+void show_matrix(const Matrix &a){
     for(int i=0;i<2;++i){
         for(int j=0;j<2;++j){
             printf("%f ",a.data[i][j].real());
