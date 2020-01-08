6aad92ea86bc842b173c47b9e0d90cc399175d30
crmccc <crmccc@126.com>
2020-01-08 08:55:09 +0800

    add commit

--- Network.h
+++ Network.h
@@ -1,44 +1,52 @@
+/*
+*Author:crmccc@126.com
+*Create time:2020.1
+*Github repo:https://github.com/crmccc/CyanFlow
+*
+*This program is licensed under the GNU General Public License v3.0
+*
+*This head file defined and implemented an network
+*
+*/
+
 #include "Eigen/Eigen"
 #include "induct.h"
 
 #ifndef _CMATH_
 #include <cmath>
-#endif 
+#endif
 
 #ifndef _GLIBCXX_COMPLEX
 #include <complex>
 #endif
-#include<future>
-#include<thread>
-#include<vector>
+#include <future>
+#include <thread>
+#include <vector>
 //ya,I know that looks stupid, but it SHUANG SI LE.
 
-#define __GII__ induct_network(i,i).real()
-#define __GIJ__ induct_network(i,j).real()
-#define __BII__ induct_network(i,i).imag()
-#define __BIJ__ induct_network(i,j).imag()
+#define __GII__ induct_network(i, i).real()
+#define __GIJ__ induct_network(i, j).real()
+#define __BII__ induct_network(i, i).imag()
+#define __BIJ__ induct_network(i, j).imag()
 #define __EI__ node[i].e
 #define __EJ__ node[j].e
 #define __FI__ node[i].f
 #define __FJ__ node[j].f
 constexpr auto LOCAL_INF = 114514;
 
-
 using namespace Eigen;
-using std::complex;
 using std::async;
+using std::complex;
 using std::future;
 using std::vector;
 /*
-*class Network.
-*OOP
+*A thread-maybe-safe,memory-consumption-may-not-big
+*power flow calculate class.
+*See cyanflow.cpp for practical example.
 */
-//!this flow calculate library obey "first PQ then PV" arrange.
-//!balance node must be the last one
 class Network
 {
 private:
-    unsigned int cpu_core_number=1u;
 public:
     /*data*/
     enum Node_type
@@ -80,12 +88,12 @@ public:
     MatrixXd delta_x;
     double delta_e_max = LOCAL_INF; //!1 1 4 5 1 4!
     double delta_f_max = LOCAL_INF; //!1 1 4 5 1 4!
-    node_arg* node;                //? nodes
+    node_arg *node;                 //? nodes
 
     /*function*/
     void gen_delta_y();
     void gen_delta_x();
-    int  gen_jacobi_part(const int start,const int end);
+    int gen_jacobi_part(const int start, const int end);
     void gen_jacobi();
     void gen_jacobi_mulit_thread(unsigned int thread_number);
     void gen_jacobi_mulit_thread();
@@ -95,7 +103,7 @@ public:
     double get_e_delta_max();
     double get_f_delta_max();
     void gen_flow();
-    complex<double>& flow(int i, int j);
+    complex<double> &flow(int i, int j);
     void gen_u();
     void init_network(int num_number);
     Network(int, int, int);
@@ -171,17 +179,17 @@ void Network::gen_delta_y()
     auto get_u_delta = [&](int i) -> double { return node[i].r * node[i].r - __EI__ * __EI__ - __FI__ * __FI__; };
     for (int i = 0; i < node_number - 1; ++i)
     {
-        if (node[i].type==Node_type::pq) //?PQ nodes.
+        if (node[i].type == Node_type::pq) //?PQ nodes.
         {
             delta_y(2 * i) = get_p_delta(i);
-            delta_y(2 * i + 1) = get_q_delta(i);                    
-            assert(2 * i + 1 < matrix_length);//debug
+            delta_y(2 * i + 1) = get_q_delta(i);
+            assert(2 * i + 1 < matrix_length); //debug
         }
-        if (node[i].type == Node_type::pv)//? PV nodes
+        if (node[i].type == Node_type::pv) //? PV nodes
         {
             delta_y(2 * i) = get_p_delta(i);
             delta_y(2 * i + 1) = get_u_delta(i);
-            assert(2 * i + 1 < matrix_length);//debug
+            assert(2 * i + 1 < matrix_length); //debug
         }
     }
 
@@ -189,38 +197,46 @@ void Network::gen_delta_y()
 }
 void Network::gen_delta_x()
 {
-     delta_x = jacobi.colPivHouseholderQr().solve(delta_y);
+    delta_x = jacobi.colPivHouseholderQr().solve(delta_y);
     //delta_x = jacobi.inverse() * delta_y;
 
     return;
 }
-void Network::gen_jacobi() {
+void Network::gen_jacobi()
+{
     gen_jacobi_part(0, node_number - 1);
     return;
 }
-void Network::gen_jacobi_mulit_thread() {
+void Network::gen_jacobi_mulit_thread()
+{
+    unsigned int cpu_core_number = std::thread::hardware_concurrency();
     gen_jacobi_mulit_thread(cpu_core_number);
     return;
 }
-void Network::gen_jacobi_mulit_thread(unsigned int thread_number) {
-    if (thread_number > cpu_core_number) {
+void Network::gen_jacobi_mulit_thread(unsigned int thread_number)
+{
+    unsigned int cpu_core_number = std::thread::hardware_concurrency();
+    if (thread_number > cpu_core_number)
+    {
         thread_number = cpu_core_number;
     }
-    if (thread_number > node_number - 1) {
+    if (thread_number > node_number - 1)
+    {
         thread_number = node_number - 1;
     }
     int per_thread = (node_number - 1) / thread_number;
-    int last_thread = node_number-1-per_thread * thread_number+per_thread;
+    int last_thread = node_number - 1 - per_thread * thread_number + per_thread;
     int start = 0;
     int end = per_thread;
     vector<future<int>> fu;
-    for (int i = 0;i < thread_number - 1;++i) {
-        fu.push_back(std::async(&Network::gen_jacobi_part,this, start, end));
+    for (int i = 0; i < thread_number - 1; ++i)
+    {
+        fu.push_back(std::async(&Network::gen_jacobi_part, this, start, end));
         start += per_thread;
         end += per_thread;
     }
-    fu.push_back(std::async(&Network::gen_jacobi_part,this, start, node_number-1));
-    for (auto& i : fu)
+    fu.push_back(std::async(&Network::gen_jacobi_part, this, start, node_number - 1));
+    for (auto &i : fu)
     {
         i.wait();
         i.get();
@@ -229,7 +245,7 @@ void Network::gen_jacobi_mulit_thread(unsigned int thread_number) {
     return;
 }
 
-int Network::gen_jacobi_part(const int start,const int end)//[start,end)
+int Network::gen_jacobi_part(const int start, const int end) //[start,end)
 {
     ////tobedone
     auto get_a_ii = [&](int i) -> double {
@@ -255,7 +271,6 @@ int Network::gen_jacobi_part(const int start,const int end)//[start,end)
         return sum;
     };
 
-
     for (int i = start; i < end; ++i)
     {
         double a = get_a_ii(i);
@@ -271,19 +286,18 @@ int Network::gen_jacobi_part(const int start,const int end)//[start,end)
             auto get_r_ii = [&](int i) -> double { return 2 * __FI__; };
             auto get_s_ii = [&](int i) -> double { return 2 * __EI__; };
 
-            if ( Node_type::pq==node[i].type) //?PQ nodes
+            if (Node_type::pq == node[i].type) //?PQ nodes
             {
 
                 if (i != j) //?non-diag
                 {
-                    /////!this can be rewriten as inline function to make it look batter.
+                    /////!this can be rewrote as inline function to make it look batter.
                     jacobi(2 * i, 2 * j) = get_h_ij(i, j);                //? witch is H_ij
                     jacobi(2 * i, 2 * j + 1) = get_n_ij(i, j);            //? witch is N_ij
                     jacobi(2 * i + 1, 2 * j) = -jacobi(2 * i, 2 * j + 1); //? witch is J_ij
                     jacobi(2 * i + 1, 2 * j + 1) = jacobi(2 * i, 2 * j);  //? witch is L_ij
-                    assert(2 * i +1 < matrix_length);//debug
-                    assert(2 * j + 1 < matrix_length);//debug
-
+                    assert(2 * i + 1 < matrix_length);                    //debug
+                    assert(2 * j + 1 < matrix_length);                    //debug
                 }
                 else //?diag
                 {
@@ -291,12 +305,11 @@ int Network::gen_jacobi_part(const int start,const int end)//[start,end)
                     jacobi(2 * i, 2 * j + 1) = get_n_ii(i);     //? witch is N_ii
                     jacobi(2 * i + 1, 2 * j) = get_j_ii(i);     //? witch is J_ii
                     jacobi(2 * i + 1, 2 * j + 1) = get_l_ii(i); //? witch is L_ii
-                    assert(2 * i + 1 < matrix_length);//debug
-                    assert(2 * j + 1 < matrix_length);//debug
-
+                    assert(2 * i + 1 < matrix_length);          //debug
+                    assert(2 * j + 1 < matrix_length);          //debug
                 }
             }
-            else if(Node_type::pv == node[i].type)//?PV nodes
+            else if (Node_type::pv == node[i].type) //?PV nodes
             {
                 //?non-diag
                 if (i != j)
@@ -305,8 +318,8 @@ int Network::gen_jacobi_part(const int start,const int end)//[start,end)
                     jacobi(2 * i, 2 * j + 1) = get_n_ij(i, j); //? witch is N_ij
                     jacobi(2 * i + 1, 2 * j) = 0;              //? witch is R_ij
                     jacobi(2 * i + 1, 2 * j + 1) = 0;          //? witch is S_ij
-                    assert(2 * i + 1 < matrix_length);//debug
-                    assert(2 * j + 1 < matrix_length);//debug
+                    assert(2 * i + 1 < matrix_length);         //debug
+                    assert(2 * j + 1 < matrix_length);         //debug
                 }
                 else //?diag
                 {
@@ -314,31 +327,35 @@ int Network::gen_jacobi_part(const int start,const int end)//[start,end)
                     jacobi(2 * i, 2 * j + 1) = get_n_ii(i);     //? witch is N_ii
                     jacobi(2 * i + 1, 2 * j) = get_r_ii(i);     //? witch is R_ii
                     jacobi(2 * i + 1, 2 * j + 1) = get_s_ii(i); //? witch is S_ii
-                    assert(2 * i + 1 < matrix_length);//debug
-                    assert(2 * j + 1 < matrix_length);//debug
+                    assert(2 * i + 1 < matrix_length);          //debug
+                    assert(2 * j + 1 < matrix_length);          //debug
                 }
             }
         }
     }
     return 0;
 }
-void Network::gen_flow() {
+void Network::gen_flow()
+{
     gen_u();
     induct_network.gen_tree();
 
-    for (int i = 0;i < node_number;++i) {
-        if (node[i].type == Node_type::balance) {//balance node power 
+    for (int i = 0; i < node_number; ++i)
+    {
+        if (node[i].type == Node_type::balance)
+        { //balance node power
             complex<double> sum;
-            for (int j = 0;j < node_number;++j) {
-                sum += induct_network[j][i] * node[j].u;
+            for (int j = 0; j < node_number; ++j)
+            {
+                sum += induct_network(j, i) * node[j].u;
             }
             sum *= complex<double>{__EI__, __FI__};
             node[i].p = sum.real();
             node[i].q = sum.imag();
-
         }
-        for (int j = 0;j < node_number;++j) {//line power loss
-            flow(i, j) = node[i].u * ((conj(node[i].u) - conj(node[j].u) )* conj(induct_network[i][j]));
+        for (int j = 0; j < node_number; ++j)
+        { //line power loss
+            flow(i, j) = node[i].u * ((conj(node[i].u) - conj(node[j].u)) * conj(induct_network(i, j)));
             total_loss += flow(i, j);
         }
     }
@@ -352,7 +369,7 @@ void Network::init_network(int num_number)
 Network::Network(int pq, int pv, int total) : balance_no(total - 1), matrix_length(2 * total - 2), pv_node_number(pv), pq_node_number(pq), induct_network(total), node_number(total),
                                               jacobi(matrix_length, matrix_length), delta_x(matrix_length, 1), delta_y(matrix_length, 1)
 {
-    cpu_core_number = std::thread::hardware_concurrency();
+    unsigned cpu_core_number = std::thread::hardware_concurrency();
     node = new node_arg[node_number];
     init_network(node_number);
 }
@@ -364,14 +381,16 @@ Network::~Network()
     //}
     //catch (const std::exception & e) {}
 }
-complex<double>& Network::flow(int i,int j) {
+complex<double> &Network::flow(int i, int j)
+{
     return (induct_network.flow)[i][j];
 }
-void Network::gen_u() {
-    for (int i = 0;i < node_number;++i) {
-    
-        node[i].u = complex<double>{ __EI__,__FI__ };
- 
+void Network::gen_u()
+{
+    for (int i = 0; i < node_number; ++i)
+    {
+
+        node[i].u = complex<double>{__EI__, __FI__};
     }
 
     return;
--- induct.h
+++ induct.h
@@ -1,3 +1,13 @@
+/*
+*Author:crmccc@126.com
+*Create time:2020.1
+*Github repo:https://github.com/crmccc/CyanFlow
+*
+*This program is licensed under the GNU General Public License v3.0
+*
+*This head file defined and implemented an inductance network
+*/
+
 #ifndef _GLIBCXX_MAP
 #include <map>
 #endif
@@ -5,101 +15,124 @@
 #include <complex>
 #endif
 #ifndef _INDUCT_CYAN_
-#define _INDUCT_CYAN_ 
-#endif 
+#define _INDUCT_CYAN_
+#endif
 
-//#include<list>
 using std::map;
+//#include<list>
 using std::complex;
 //using std::list;
 
-//!THE FIRST NODE IS MARKED AS 0,NOT 1!
 /*
-*
-* inductance ---> node
-*
-* --data: a list of maps of int and complex double.
-*
+*!THE FIRST NODE IS MARKED AS 0,NOT 1!
+*This class genurate inductance network for power network power flow calculate.
+*This class is specifically designed for the specific lession.
+*Example: 
+*   induct ind(node_number);
+*   ind.add_line({1,4},1,1);
 */
 class induct
 {
 private:
-    complex<double> zero_complex{ 0,0 };
-    map<int,complex<double> >* inductance;
+    complex<double> zero_complex{0, 0};    //To avoid the cost of copy complex,make a zero.
+    map<int, complex<double>> *inductance; //The data of the inductance network.
 public:
-    bool **book;
-    
+    bool **book; //whether i,j has a line or not?
+
     // bool book[100][100]{0};
-    int node_number;
-     //vector<map<int, complex<double>>> inductance;
-     //complex<double>** inductance;
-     complex<double>** flow;
-     int start;
+    int node_number; //as its name suggested
+    //vector<map<int, complex<double>>> inductance;
+    //complex<double>** inductance;
+    complex<double> **flow; //the flow data
+    int start;
+    //The start point of the network ,mainly used in generating hte tree of the network.
     //complex<double> inductance[100][100];
-    
-    int add_line(complex<double>& , int , int );
-    map<int, complex<double>>& operator[](int i);
-    complex<double>& operator()(int i, int j);
+
+    int add_line(complex<double> &, int, int);
+    map<int, complex<double>> &operator[](int i);
+    complex<double> &operator()(int i, int j);
     void gen_tree();
 
     induct(int);
     ~induct();
 };
-complex<double>& induct::operator()(int i, int j) {
-    if (book[i][j] == false&&i!=j) {
+//Overload the () operator for a batter user experience.
+//Now you can get the inductance of two point like that:
+//  induct inductance(node_number);
+//  cout<<inductance(1,5);
+complex<double> &induct::operator()(int i, int j)
+{
+    if (book[i][j] == false && i != j)
+    {
         return zero_complex;
     }
-    else {
+    else
+    {
         return inductance[i][j];
     }
-
 }
-map<int ,complex<double>>& induct::operator[](int i)
+//Overload [] operator for a batter user experience.
+//Legency.
+map<int, complex<double>> &induct::operator[](int i)
 {
     return inductance[i];
 }
+//A tree generator (or plan to be).
+//I soon realized that it's not necessary.
+//Now it will find the start point of the network.
+//Use a typical tree generate algorithm.
 inline void induct::gen_tree()
 {
-    int* tree_book = new int[node_number] {0};
-    for (int i = 0;i < node_number;++i) {
-        for (int j = 0;j < node_number;++j) {
-            if (book[i][j]) {
+    int *tree_book = new int[node_number]{0};
+    for (int i = 0; i < node_number; ++i)
+    {
+        for (int j = 0; j < node_number; ++j)
+        {
+            if (book[i][j])
+            {
                 ++tree_book[i];
             }
         }
     }
-    for (start=0;start < node_number;++start) {
-        if (tree_book[start] == 1) {
+    for (start = 0; start < node_number; ++start)
+    {
+        if (tree_book[start] == 1)
+        {
             break;
         }
     }
     if (start >= node_number)
-        start = node_number-1;
-//    delete[] tree_book;
+        start = node_number - 1;
+    //    delete[] tree_book;
 
     return;
 }
-int induct::add_line(complex<double>& ind, int a, int b)
+//The main generate function of the class.
+//ind: the resistance of the line
+//a,b: 2 ends of the line
+//example :
+//  ind.add_line({1,0},1,1);
+int induct::add_line(complex<double> &ind, int a, int b)
 {
-    inductance[a][b] -= 1.0/ind;
-    inductance[b][a] -= 1.0/ind;
-    inductance[a][a] += 1.0/ind;
-    inductance[b][b] += 1.0/ind;
+    inductance[a][b] -= 1.0 / ind;
+    inductance[b][a] -= 1.0 / ind;
+    inductance[a][a] += 1.0 / ind;
+    inductance[b][b] += 1.0 / ind;
     book[a][b] = book[b][a] = 1; //! self-self will not be counted.
     return 0;
 }
 induct::induct(int node_number) : node_number(node_number)
 {
-     book = new bool* [node_number];
-     inductance = new map<int,complex<double>> [node_number];
-     flow = new complex<double> * [node_number];
+    book = new bool *[node_number];
+    inductance = new map<int, complex<double>>[node_number];
+    flow = new complex<double> *[node_number];
 
-     for (int i = 0; i < node_number; ++i)
+    for (int i = 0; i < node_number; ++i)
     {
-         //inductance[i] = new complex<double> [node_number];
-         flow[i] = new complex<double>[node_number];
-         book[i] = new bool[node_number]{0};
-         //memset(book[i], 0, node_number * sizeof(bool));
+        //inductance[i] = new complex<double> [node_number];
+        flow[i] = new complex<double>[node_number];
+        book[i] = new bool[node_number]{0};
+        //memset(book[i], 0, node_number * sizeof(bool));
     }
 }
 
